a1b882a02cd4ec31664c452ce48073bcf620ddf2
M-Lenvik <lenvik.marie@gmail.com>
2026-01-25 13:54:07 +0100

    perf: self-host fonts to reduce render blocking

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { PlanProvider } from "./context/PlanContext";
 import { router } from "./Router";
+import "@fontsource/combo/400.css";
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/600.css";
+import "@fontsource/noto-sans/700.css";
+import "@fontsource/sour-gummy/400.css";
+import "@fontsource/sour-gummy/600.css";
+import "@fontsource/sour-gummy/700.css";
+import "@fontsource/sour-gummy/800.css";
 import "./Styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
